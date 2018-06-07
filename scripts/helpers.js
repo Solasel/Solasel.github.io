d86699f8e2eec259ed837f33ffac18ecb1ea7ab0
Will <wewalker@berkeley.edu>
2018-06-07 06:48:24 -0700

    redo filestructure, change output
    make the information page the default page, and the tool page open a new window to which it prints the output.

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,3 +1,21 @@
-function clear() {
-	document.write("HENLO");
+function output() {
+	"use strict";
+	var win = window.open();
+	win.document.write('<!DOCTYPE html>' +
+		'<html>' +
+
+		'<head>' +
+			'<title>Slayer Tool Tips</title>' +
+			'<link rel="stylesheet" type="text/css" href="../styles/main.css">' +
+			'<meta charset="UTF-8">' +
+			'<meta name="description" content="Your Output!">' +
+			'<meta name="author" content="William Walker">' +
+			'<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+		'</head>' +
+
+		'<body style="padding: 1vh 1vw;">' +
+			'<span>' + alg() + '</span>' +
+		'</body>' +
+
+		'</html>');
 }
