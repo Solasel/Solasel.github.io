8c6646a3d038ec053d32f1e7cb6c2901ab779eba
Will <wewalker@berkeley.edu>
2018-06-07 18:38:56 -0700

    remove head, see how it affects

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -5,12 +5,7 @@ function output() {
 		'<html>' +
 
 		'<head>' +
-			'<title>Results</title>' +
-			'<link rel="stylesheet" type="text/css" href="../styles/main.css">' +
-			'<meta charset="UTF-8">' +
-			'<meta name="description" content="Your Output!">' +
-			'<meta name="author" content="William Walker">' +
-			'<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+			
 		'</head>' +
 
 		'<body style="padding: 1vh 1vw; overflow: scroll">' +
